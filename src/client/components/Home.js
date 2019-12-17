b884e21e5eae82b32816bd174e4b3e457d84f1be
Egor Proskuryakov <egpro92@gmail.com>
2019-12-17 16:08:19 +1000

    Init

---
+++ src/client/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <div>Fuch this world!</div>
+    <button onClick={() => console.log("Hi there!")}>Press me!</button>
+  </div>
+);
+
+export default Home;
