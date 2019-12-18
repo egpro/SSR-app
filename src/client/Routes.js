6baf75bd708136249385b004dfdba1495abb76a6
Egor Proskuryakov <egpro92@gmail.com>
2019-12-18 15:02:35 +1000

    Implement isomorphyc routing

---
+++ src/client/Routes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+
+export default () => (
+  <div>
+    <Route exact path="/" component={Home} />
+    <Route path="/hi" component={() => "HI!"} />
+  </div>
+);
