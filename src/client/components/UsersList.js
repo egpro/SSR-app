97c4d4bb38c3cb3f872b9f754e751fa968139ca0
Egor Proskuryakov <egpro92@gmail.com>
2019-12-31 13:28:46 +1000

    add redux store

---
+++ src/client/components/UsersList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../actions";
+
+class UserList extends React.Component {
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUsers() {
+    return this.props.users.map(user => {
+      return <li key={user.id}>{user.name}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Ha ha:
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    );
+  }
+}
+
+const mapState = state => ({
+  users: state.users
+});
+
+export default connect(mapState, { fetchUsers })(UserList);
